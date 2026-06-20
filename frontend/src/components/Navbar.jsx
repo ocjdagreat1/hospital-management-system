462e5b3e5686f958a8cfd7d30ac9e2a7b983b0d8
ocjdagreat1 <oparachibuikejustine@gmail.com>
2026-06-20 15:52:39 +0100

    added admin url

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
         {/* ✅ Admin Panel Button - show only on home page  */}
         {location.pathname === '/' && (
           <button
-            onClick={() => window.open('https://appointy-six.vercel.app', '_blank')}
+            onClick={() => window.open('https://hospital-management-system-flax-two.vercel.app/', '_blank')}
             className='bg-primary text-white text-xs px-4 py-2 rounded-full hover:bg-gray-700 hidden md:block'
           >
             Admin Panel
